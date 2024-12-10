292ea4c3ab5e83d1d3205d509567196514e0e6d7
Vineet Singraul <Singraulvineet20@gmail.com>
2024-12-10 21:14:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// replace(java/ig,"paython")
+// its convert the 
+// i = casesenstive
+// g = globale maje any variable
+
+function replaceed(){
+    text = "java is the be st Java can changes the world Java :";
+    let ane = text.replace(/java/ig,"Paython")
+    alert(ane)
+}
